5a62900519cd6c1347299eb3a36e5d94d411058c
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-09-10 22:58:29 +0530

    back to localhost

--- src/redux/actions/authService.js
+++ src/redux/actions/authService.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 // export const API_URL = 'http://127.0.0.1:8000/account/api/';
 
 // export const API = 'http://127.0.0.1:8000/account/';
-export const BASE_URL = 'https://weatherstation-server.onrender.com/'
-export const B_URL = 'https://weatherstation-server.onrender.com'
+
+export const BASE_URL = 'http://127.0.0.1:8000/'
+export const B_URL = 'http://127.0.0.1:8000'
 
 // export const API_URL_PROFIL = 'http://127.0.0.1:8000/profiles/'
 
